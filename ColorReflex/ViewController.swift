5cd48cc3994d124b246afd18b682f77891b17986
asronline <asronline@gmail.com>
2015-01-25 19:08:45 +0000

    Added new game button, UI fixes

--- ColorReflex/ViewController.swift
+++ ColorReflex/ViewController.swift
@@ -11,21 +11,24 @@ import Foundation
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var redButton: UIButton!
     @IBOutlet weak var greenButton: UIButton!
     @IBOutlet weak var blueButton: UIButton!
     @IBOutlet weak var yellowButton: UIButton!
     @IBOutlet weak var colorLabel: UILabel!
+    @IBOutlet weak var newGameButton: UIButton!
     
     let colors = ["red", "green","blue","yellow"]
     var score = 0;
     
-    var secondsLeft: Int = 3
+    var secondsLeft = 5
     var timer: NSTimer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         self.startTimer()
         updateColorLabel()
     }
@@ -61,8 +64,15 @@ class ViewController: UIViewController {
     func resetScore() {
         score = 0
         scoreLabel.text = String(score)
+        newGameButton.hidden = false
     }
     
+    @IBAction func startNewGame(sender: AnyObject) {
+        secondsLeft = 5
+        startTimer()
+        updateColorLabel()
+        newGameButton.hidden = true
+    }
     
     @IBAction func colourTapped(sender: UIButton) {
         if (colorLabel.text == "green" && sender == greenButton) ||
@@ -70,11 +80,12 @@ class ViewController: UIViewController {
             (colorLabel.text == "blue" && sender == blueButton) ||
             (colorLabel.text == "yellow" && sender == yellowButton) {
                 scorePoint()
+                updateColorLabel()
+                
         } else {
             resetScore()
+            titleLabel.text = "GAME OVER!"
         }
-        
-        updateColorLabel()
     }
     
     override func didReceiveMemoryWarning() {
